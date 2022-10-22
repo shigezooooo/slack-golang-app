b05d81336298e13962f00752b41901de8aeb0fba
shigezoooo <y-shigemitsu@pscsrv.co.jp>
2022-10-22 10:30:18 +0900

    Update main.go

--- get-news/main.go
+++ get-news/main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"os"
-	"strings"
-	"strconv"
+	"encoding/json"
 	"log"
 	"net/url"
-	"encoding/json"
-	"github.com/mmcdole/gofeed"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/slack-go/slack"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/mmcdole/gofeed"
+	"github.com/slack-go/slack"
 )
 
 /// リクエスト
 type RequestBody struct {
 	Challenge string `json:"challenge"`
-	Token string `json:"token"`
-	Type string `json:"type"`
-	Event Event `json:"event"`
+	Token     string `json:"token"`
+	Type      string `json:"type"`
+	Event     Event  `json:"event"`
 }
 
 type Event struct {
-	Text string `json:"text"`
+	Text    string `json:"text"`
 	Channel string `json:"channel"`
 }
 
@@ -35,13 +35,13 @@ type ChallengeResponseBody struct {
 }
 
 type ResponseBody struct {
-	Status int `json:"status"`
+	Status int    `json:"status"`
 	Detail string `json:"detail"`
 }
 
 type NewsItem struct {
-	Title  string `json:"title"`
-	Link   string `json:"link"`
+	Title string `json:"title"`
+	Link  string `json:"link"`
 }
 
 type NewsItems []NewsItem
@@ -49,99 +49,81 @@ type NewsItems []NewsItem
 /// lambdaハンドラー
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	// リクエストBody 取得
 	requestBody, err := getRequestBody(request.Body)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
-			Body: err.Error(),
+			Body:       err.Error(),
 			StatusCode: 500,
 		}, err
 	}
 
+	event := requestBody.Event
+	challenge := requestBody.Challenge
+
 	// challenge認証
-	if requestBody.Challenge != "" {
+	if challenge != "" {
 		responseBody, err := json.Marshal(ChallengeResponseBody{
-			Challenge: requestBody.Challenge,
+			Challenge: challenge,
 		})
 
 		_ = err
 		return events.APIGatewayProxyResponse{
-			Body: string(responseBody),
+			Body:       string(responseBody),
 			StatusCode: 200,
 		}, nil
 	}
 
 	if request.Headers["X-Slack-Signature"] != "" {
 		// slackからの投稿の場合、SlackBotに取得結果をPOSTする
-		postChannel(requestBody)
+
+		channel := event.Channel
+		textArray := strings.Split(event.Text, "\n")
+
+		log.Print(textArray)
+		if len(textArray) < 2 {
+			// パラメータが不足
+			sendSlackMessage(channel, "検索ワードを検出できませんでした")
+
+			responseBody, err := json.Marshal(ResponseBody{
+				Status: 1,
+				Detail: "NG",
+			})
+
+			_ = err
+			return events.APIGatewayProxyResponse{
+				Body:       string(responseBody),
+				StatusCode: 200,
+			}, nil
+		}
+
+		query := textArray[1]
+
+		// RSSフィードでニュースを取得
+		log.Print("検索ワード：" + query)
+		urlText := "https://news.google.com/rss/search?hl=ja&gl=JP&ceid=JP:ja&q=" + url.QueryEscape(query)
+		fp := gofeed.NewParser()
+		feed, _ := fp.ParseURL(urlText)
+
+		message := query + " に関するニュースをお届けします! \n\n" + feed.Items[0].Title + " \n" + feed.Items[0].Link + " \n\n"
+
+		sendSlackMessage(channel, message)
 	}
 
-	// レスポンスBody 生成
 	responseBody, err := json.Marshal(ResponseBody{
 		Status: 0,
-		Detail: "正常終了",
+		Detail: "OK",
 	})
 
 	return events.APIGatewayProxyResponse{
-		Body: string(responseBody),
+		Body:       string(responseBody),
 		StatusCode: 200,
 	}, nil
 }
 
-func postChannel(body *RequestBody) {
-	log.Print(body.Event.Text)
-	
-	// POST値チェック
-	channel := body.Event.Channel
-	textArray := strings.Split(body.Event.Text, "\n")
-	limitStr := ""
-	log.Print(textArray)
-	if len(textArray) >= 3 {
-		// 正常
-		limitStr = textArray[2]
-	} else if len(textArray) == 2 {
-		// 正常（limitにデフォルト値を設定）
-		limitStr = getEnv("DEFAULT_LIMIT_CNT", "1")
-	} else {
-		// 異常
-		sendSlackMessage(channel, "検索ワードを入力してね")
-		return
-	} 
-
-	query := textArray[1]
-	limit, err := strconv.Atoi(limitStr)
-	if err != nil {
-		// 変換に失敗
-		log.Print(err.Error())
-		sendSlackMessage(channel, "取得件数は1以上の整数で入力してね")
-		return
-	}
-
-	log.Print(query)
-	log.Print(limit)
-
-	// RSSフィードを取得
-	urlText := "https://news.google.com/rss/search?hl=ja&gl=JP&ceid=JP:ja&q=" + url.QueryEscape(query)
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(urlText)
-
-	text := query + " に関するニュースをお届けします! \n\n"
-	for cnt, item := range feed.Items {
-		text += item.Title + " \n" + item.Link + " \n\n"
-
-		if cnt + 1 == limit {
-			break
-		}
-	}
-
-	// slackへメッセージをPOST
-	sendSlackMessage(channel, text)
-}
-
+/// Slackへメッセージを投稿する
 func sendSlackMessage(channel, text string) {
-	log.Print(text)
-
-	res, err := fetchParameterStore("SlackGolangApp-SlackAccessToken")
+	// パラメータストアより認証キーを取得
+	res, err := fetchParameterStore(getEnv("SSM_SLACK_AUTH_KEY_NAME", ""))
 	if err != nil {
 		log.Print(err)
 		return
@@ -155,13 +137,7 @@ func sendSlackMessage(channel, text string) {
 	}
 }
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
+/// APIGatewayからのリクエストBodyを処理用に変換する
 func getRequestBody(bodyText string) (*RequestBody, error) {
 	var body RequestBody
 	err := json.Unmarshal([]byte(bodyText), &body)
@@ -171,6 +147,7 @@ func getRequestBody(bodyText string) (*RequestBody, error) {
 	return &body, nil
 }
 
+/// パラメータストアから値を取得する
 func fetchParameterStore(paramName string) (string, error) {
 
 	sess := session.Must(session.NewSession())
@@ -180,15 +157,23 @@ func fetchParameterStore(paramName string) (string, error) {
 	)
 
 	res, err := svc.GetParameter(&ssm.GetParameterInput{
-		Name: aws.String(paramName),
+		Name:           aws.String(paramName),
 		WithDecryption: aws.Bool(true),
 	})
 	if err != nil {
-		return "SSM：パラメータ取得失敗", err
+		return "SSM: パラメータ取得失敗", err
 	}
 
 	value := *res.Parameter.Value
 	return value, nil
+}
+
+/// Lambdaに設定した環境変数を参照する
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 func main() {
